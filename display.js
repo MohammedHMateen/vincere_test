12db316deebc3fde0c1af905e65462af07215d25
MohammedHMateen <myfriendhammad@gmail.com>
2020-10-19 05:51:31 +0530

    Added styling and fixed a logical error

--- display.js
+++ display.js
@@ -1,7 +1,9 @@
 let data_string = localStorage.getItem('Student_Data');
-
 let saved_student= JSON.parse(data_string);
 
 
 
 document.getElementById("name").innerHTML = saved_student.name;
+document.getElementById("age").innerHTML = saved_student.age;
+let avg = (parseInt(saved_student.m1) + parseInt(saved_student.m2) +parseInt(saved_student.m3) +parseInt (saved_student.m4)+parseInt(saved_student.m5))/5.0;
+document.getElementById("avg").innerHTML = avg;
